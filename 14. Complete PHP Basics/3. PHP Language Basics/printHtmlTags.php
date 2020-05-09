fcb0c156c6323213f5faec1461c7efbfdd8dc162
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-05-09 11:25:19 +0530

    php basics

---
+++ 14. Complete PHP Basics/3. PHP Language Basics/printHtmlTags.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print "<p>Hello World From Print</p>"
+    ?>
+        <h1>
+        <?php
+            echo "hello From The Heading"
+        ?>
+    </h1>
+</body>
+</html>
